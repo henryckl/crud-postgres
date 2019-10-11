ebdfd4f51f48efc4d494641607d7d58f1e29755e
Henryck <henryck.leandro@gmail.com>
2019-10-11 03:27:57 -0300

    virificar se ja existe o email

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -2,7 +2,14 @@ const { User } = require("../models");
 
 class UserController {
   async store(req, res) {
-    console.log(User);
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+    if (userExists) {
+      console.log("existe");
+      return res
+        .status(400)
+        .json({ error: `O Usuario com email ${req.body.email} ja existe` });
+    }
+
     const { id, name, email } = await User.create(req.body);
 
     return res.json({
