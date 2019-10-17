a9ae390f535bb767cf8166f7ed0892223746e835
Henryck <henryck.leandro@gmail.com>
2019-10-17 11:42:49 -0300

    user update

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -18,6 +18,26 @@ class UserController {
       email
     });
   }
+
+  async update(req, res) {
+    const { email, name } = req.body;
+    const user = await User.findByPk(req.headers.userid);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: `O Usuario ja existe` });
+      }
+    }
+
+    const { id } = await user.update({ name, email });
+    return res.json({
+      id,
+      name,
+      email
+    });
+  }
 }
 
 module.exports = new UserController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,5 +5,6 @@ const UserController = require("./app/controllers/UserController");
 const routes = new Router();
 
 routes.post("/users", UserController.store);
+routes.put("/users", UserController.update);
 
 module.exports = routes;
